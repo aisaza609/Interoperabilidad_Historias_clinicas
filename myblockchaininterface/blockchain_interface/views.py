71469ac764ed040b91afce9cb1f60acd9a1511d2
aisaza609 <100968646+aisaza609@users.noreply.github.com>
2024-11-14 11:56:52 -0500

    Create views.py

---
+++ myblockchaininterface/blockchain_interface/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def view_records(request):
+    return render(request, 'blockchain_interface/view_records.html')
+
+def edit_record(request):
+    return render(request, 'blockchain_interface/edit_record.html')
+
+def view_history(request):
+    return render(request, 'blockchain_interface/history.html')
+
+def add_record(request):
+    return render(request, 'blockchain_interface/add_record.html')
