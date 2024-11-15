617f077c302057e3770dbca23eba011839123b4c
aisaza609 <100968646+aisaza609@users.noreply.github.com>
2024-11-15 09:01:34 -0500

    Update urls.py

--- myblockchaininterface/blockchain_interface/urls.py
+++ myblockchaininterface/blockchain_interface/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),  # Página principal
     path('view/', views.view_records, name='view_records'),
     path('edit/', views.edit_record, name='edit_record'),
     path('history/', views.view_history, name='view_history'),
