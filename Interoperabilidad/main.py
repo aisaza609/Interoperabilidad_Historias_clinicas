3568b445f506fe9671eeb200ae22da2f1af42262
jdnoguera12 <jdnoguera@unicauca.edu.co>
2024-11-19 12:19:37 -0500

    Update main.py

--- Interoperabilidad/main.py
+++ Interoperabilidad/main.py
@@ -13,7 +13,7 @@ import subprocess
 import warnings
 warnings.filterwarnings('ignore')
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-
+##holaaaa
 init(convert=True)
 
 with open("Config.yaml", "r") as yamlfile:
