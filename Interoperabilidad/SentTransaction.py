d7c2ef0b28560e659aa2722a0f2880c7d7286976
jdnoguera12 <jdnoguera@unicauca.edu.co>
2024-11-21 10:29:20 -0500

    sent finished

--- Interoperabilidad/SentTransaction.py
+++ Interoperabilidad/SentTransaction.py
@@ -1,63 +1,73 @@
 import os
-import random
-import time
 import sys
 import argparse
-#holaaa
 import yaml
+from colorama import Fore, Style
 import Miscellaneus as MS
-from colorama import Fore
-from colorama import Style
-from colorama import init
-import subprocess
-import warnings
-warnings.filterwarnings('ignore')
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-parser = argparse.ArgumentParser(
-            prog='myprogram',
-            description='''Universidad del Cauca - MINTIC Datos a la U''',
-            epilog="""Soluciones Innovadoras con Datos Abiertos.""", allow_abbrev=False)
-parser.add_argument('-s', '-S', metavar='\b', help='Guardar. Uso -s/S id_del_paciente diagnostico medicamentos control')
-parser.add_argument('-c', '-C', metavar='\b', help='Consultar. Uso -c/C id_del_paciente')
-parser.add_argument('-hi', '-HI', metavar='\b', help='Historia. Uso -hi/HI id_del_paciente')
-parser.add_argument('-sispro', '-SISPRO', metavar='\b', help='IPS/EPS. Uso -sispro/SISPRO id_SISPRO nombre')
-args = parser.parse_args()
+def load_config():
+    """Carga las configuraciones desde Config.yaml"""
+    try:
+        with open("Config.yaml", "r") as yamlfile:
+            data = yaml.load(yamlfile, Loader=yaml.FullLoader)
+            MS.printed(message="CONFIGURATION FILE LOADED SUCCESSFULLY", colour=Fore.GREEN, style=Style.BRIGHT)
+            print(MS.__file__)
+            return data[0]['Details']
+    except FileNotFoundError:
+        MS.printed(message="CONFIG FILE NOT FOUND!", colour=Fore.RED, style=Style.BRIGHT)
+        sys.exit(1)
+    except yaml.YAMLError as exc:
+        MS.printed(message=f"ERROR IN CONFIG FILE: {exc}", colour=Fore.RED, style=Style.BRIGHT)
+        sys.exit(1)
 
+def main():
+    """Punto de entrada principal del script"""
+    # Definimos los argumentos CLI disponibles
+    parser = argparse.ArgumentParser(
+        prog='SentTransaction',
+        description='''Universidad del Cauca - MINTIC Datos a la U''',
+        epilog="""Soluciones Innovadoras con Datos Abiertos."""
+    )
+    parser.add_argument('-rp', metavar=('\b', '\b', '\b'), nargs=3, help='Registrar paciente. Uso: -rp id_paciente nombre id_ips')
+    parser.add_argument('-c', metavar='\b', help='Consultar. Uso: -c id_paciente')
+    parser.add_argument('-hi', metavar='\b', help='Historial. Uso: -hi id_paciente')
+    parser.add_argument('-sispro', metavar=('\b', '\b'), nargs=2, help='Registrar IPS/EPS. Uso: -sispro id nombre')
+    args = parser.parse_args()
 
-def configfileload():
-    with open("Config.yaml", "r") as yamlfile:
-        data = yaml.load(yamlfile, Loader=yaml.FullLoader)
-        MS.printed(message="CONFIGURATION FILE LOADED SUCCESSFULLY", colour=Fore.GREEN, style=Style.BRIGHT)
+    # Cargamos configuraciones
+    config = load_config()
 
-    MS.kup = data[0]['Details']['KeepUp']
-    MS.fu.cctipo = data[0]['Details']['SmartContract']
-    MS.SCFolder = data[0]['Details']['SCFolder']
-    MS.fu.vrs = data[0]['Details']['Version']
+    # Lógica basada en los argumentos
+    if args.rp:
+        try:
+            MS.registerPatient(args.rp[0], args.rp[1], args.rp[2])
+        except Exception as e:
+            MS.printed(message=f"ERROR REGISTERING PATIENT: {e}", colour=Fore.RED, style=Style.BRIGHT)
+            sys.exit(1)
 
-if __name__ == "__main__":
-    keys = ('diagnostico', 'medicamentos', 'control')
-    datos = dict.fromkeys(keys)
+    elif args.c:
+        try:
+            MS.queryPatient(args.c)
+        except Exception as e:
+            MS.printed(message=f"ERROR QUERYING PATIENT: {e}", colour=Fore.RED, style=Style.BRIGHT)
+            sys.exit(1)
 
-    if "-s" in sys.argv[0] or "-S" in sys.argv[0]:
-        configfileload()
-        datos['diagnostico'] = sys.argv[2]
-        datos['medicamentos'] = sys.argv[3]
-        datos['control'] = sys.argv[4]
-        MS.updatepatient(sys.argv[1], datos)
+    elif args.hi:
+        try:
+            MS.getPatientHistory(args.hi)
+        except Exception as e:
+            MS.printed(message=f"ERROR FETCHING PATIENT HISTORY: {e}", colour=Fore.RED, style=Style.BRIGHT)
+            sys.exit(1)
 
-    elif "-c" in sys.argv[0] or "-C" in sys.argv[0]:
-        configfileload()
-        MS.patientconsult(sys.argv[1])
+    elif args.sispro:
+        try:
+            MS.registerIPS(args.sispro[0], args.sispro[1])
+        except Exception as e:
+            MS.printed(message=f"ERROR REGISTERING IPS: {e}", colour=Fore.RED, style=Style.BRIGHT)
+            sys.exit(1)
 
-    elif "-hi" in sys.argv[0] or "-HI" in sys.argv[0]:
-        configfileload()
-        MS.patienhistory(sys.argv[1])
-
-    elif "-sispro" in sys.argv[0] or "-SISPRO" in sys.argv[0]:
-        configfileload()
-        MS.addIEPS(sys.argv[1], sys.argv[2])
-
-    elif "" in sys.argv[0] or "-h" in sys.argv[0] or "--help" in sys.argv[0]:
+    else:
         parser.print_help()
 
+if __name__ == "__main__":
+    main()
